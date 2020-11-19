ec8fdedfad37487ad81c4610aba6b39cb0ea1541
ShravyaHub <shravyak710@gmail.com>
2020-11-19 22:09:27 +0530

    ADDED: code to retrieve data within a certain date range

--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -1,6 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class AddressBookTest {
@@ -19,6 +20,15 @@ public class AddressBookTest {
         addressBookService.updateContactAddress("Sudha", "Jayanagar");
         boolean result = addressBookService.checkAddressBookInSyncWithDatabase("Sudha");
         Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenAddressBookData_ShouldReturnNumberOfContactsWithinDateRange() throws AddressBookException {
+        AddressBookService addressBookService = new AddressBookService();
+        LocalDate start = LocalDate.of(2020, 03, 01);
+        LocalDate end = LocalDate.of(2020, 04, 30);
+        List<PersonData> addressBookData = addressBookService.readAddressBookData(start, end);
+        Assert.assertEquals(2, addressBookData.size());
     }
 
 }
